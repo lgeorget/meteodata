2675373541706be820d5719a0eed9448a4233b4b
Laurent Georget <laurent.georget@inria.fr>
2015-05-17 19:52:05 +0200

    VantagePro2Collectors: Add function stubs

--- Meteodata/Collectors/VantagePro2Collectors.pm
+++ Meteodata/Collectors/VantagePro2Collectors.pm
@@ -1,5 +1,39 @@
 package Meteodata::Collectors::VantagePro2Collectors;
 
+use Moo;
+
+has "console" {
+	is => 'ro',
+}
+
+sub getOneRound {
+	my $self = shift;
+
+}
+
+sub write {
+}
+
+sub wake_up {
+	my $self = shift;
+
+	my $answer;
+	foreach (1..3) {
+		$self->write("\n");
+		sysread($self->console, $answer, 2);
+		if ($answer != "\n\r") {
+			sleep 2; #wait before retrying
+		} else {
+			last; #succesfully woken up, jump out of the loop
+		}
+	}
+}
+
+sub check_crc {
+	my $self = shift;
+	my $blob = shift;
+	my $crc = shift;
+}
 
 
 1;

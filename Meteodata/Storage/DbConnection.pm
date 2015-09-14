7faae5a3fe85e47490eee1cf48a4884bbdc3acc5
Laurent Georget <laurent.georget@inria.fr>
2015-09-14 22:00:02 +0200

    Complete INSERT command

--- Meteodata/Storage/DbConnection.pm
+++ Meteodata/Storage/DbConnection.pm
@@ -66,7 +66,61 @@ sub DEMOLISH {
 
 sub add_new_data {
 	my ($self,$id,$data) = @_;
-	my $put_stmt = $Meteodata::Storage::db->query("INSERT INTO data_$id (...) VALUES (...)")->get;
+	my $now = localtime;
+	my $put_stmt = $Meteodata::Storage::db->query("
+	INSERT INTO meteo (
+	station,time,Bar_Trend,Barometer,Inside_Temperature,
+	Inside_Humidity,Outside_Humidity,
+	Extra_Temperatures_1,Extra_Temperatures_2, Extra_Temperatures_3,
+	Extra_Temperatures_4, Extra_Temperatures_5, Extra_Temperatures_6,
+	Extra_Temperatures_7, Soil_Temperatures_1, Soil_Temperatures_2,
+	Soil_Temperatures_3, Soil_Temperatures_4, Leaf_Temperatures_1,
+	Leaf_Temperatures_2, Leaf_Temperatures_3, Leaf_Temperatures_4,
+	Extra_Humidities_1, Extra_Humidities_2, Extra_Humidities_3,
+	Extra_Humidities_4, Extra_Humidities_5, Extra_Humidities_6,
+	Extra_Humidities_7, Soil_Moistures_1, Soil_Moistures_2,
+	Soil_Moistures_3, Soil_Moistures_4, Leaf_Wetnesses_1, Leaf_Wetnesses_2,
+	Leaf_Wetnesses_3, Leaf_Wetnesses_4, Wind_Speed, Wind_Direction,
+	Ten_Min_Avg_Wind_Speed, Two_Min_Avg_Wind_Speed, Ten_Min_Wind_Gust,
+	Rain_Rate, Last_15_min_Rain, Last_Hour_Rain, Last_24_Hour_Rain,
+	Day_Rain, Month_Rain, Year_Rain, Storm_Rain,
+	Start_Date_of_Current_Storm, UV, Solar_Radiation, Dew_Point,
+	Heat_Index, Wind_Chill, THSW_Index, Day_ET, Month_ET, Year_ET,
+	Time_Sunrise, Time_Sunset, Console_Battery_Voltage
+	)
+	VALUES (
+	$id,$now,
+	$data->{'Bar Trend'},$data->{'Barometer'},$data->{'Inside Temperature'},
+	$data->{'Inside Humidity'},$data->{'Outside Humidity'},
+	$data->{'Extra Temperatures'}[1], $data->{'Extra Temperatures'}[2],
+	$data->{'Extra Temperatures'}[3], $data->{'Extra Temperatures'}[4],
+	$data->{'Extra Temperatures'}[5], $data->{'Extra Temperatures'}[6],
+	$data->{'Extra Temperatures'}[7],
+	$data->{'Soil Temperatures'}[1], $data->{'Soil Temperatures'}[2],
+	$data->{'Soil Temperatures'}[3], $data->{'Soil Temperatures'}[4],
+	$data->{'Leaf Temperatures'}[1], $data->{'Leaf Temperatures'}[2],
+	$data->{'Leaf Temperatures'}[3], $data->{'Leaf Temperatures'}[4],
+	$data->{'Extra Humidities'}[1], $data->{'Extra Humidities'}[2],
+	$data->{'Extra Humidities'}[3], $data->{'Extra Humidities'}[4],
+	$data->{'Extra Humidities'}[5], $data->{'Extra Humidities'}[6],
+	$data->{'Extra Humidities'}[7],
+	$data->{'Soil Moistures'}[1], $data->{'Soil Moistures'}[2],
+	$data->{'Soil Moistures'}[3], $data->{'Soil Moistures'}[4],
+	$data->{'Leaf Wetnesses'}[1], $data->{'Leaf Wetnesses'}[2],
+	$data->{'Leaf Wetnesses'}[3], $data->{'Leaf Wetnesses'}[4],
+	$data->{'Wind Speed'}, $data->{'Wind Direction'},
+	$data->{'10-Min Avg Wind Speed'}, $data->{'2-Min Avg Wind Speed'},
+	$data->{'10-Min Wind Gust'},
+	$data->{'Rain Rate'}, $data->{'Last 15-min Rain'},
+	$data->{'Last Hour Rain'}, $data->{'Last 24-Hour Rain'},
+	$data->{'Day Rain'}, $data->{'Month Rain'}, $data->{'Year Rain'},
+	$data->{'Storm Rain'}, $data->{'Start Date of Current Storm'},
+	$data->{'UV'}, $data->{'Solar Radiation'}, $data->{'Dew Point'},
+	$data->{'Heat Index'}, $data->{'Wind Chill'}, $data->{'THSW Index'},
+	$data->{'Day ET'}, $data->{'Month ET'}, $data->{'Year ET'},
+	$data->{'Time Sunrise'}, $data->{'Time Sunset'},
+	$data->{'Console Battery Voltage'}
+	)")->get;
 	my ($type, $result) = $put_stmt->execute($data);
 }
 
